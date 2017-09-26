9372f35431fd4d332e789f0f4d8d916353718f0e
jay-shi <j67shi@edu.uwaterloo.ca>
2017-09-26 16:19:36 -0400

    add endpoints in bff

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
  
-app.get('/backend', function(req, res){
+app.get('/test', function(req, res){
+    console.log(req.body)
     res.send('success!');
-})
+});
 
+app.post('/backend', function(req, res){
+    console.log(req.body)
+    res.send('success!');
+});
 app.listen(3000);
