997bd70a9f0cf47d47993686eddc7111ad75149e
jay-shi <j67shi@edu.uwaterloo.ca>
2017-09-28 01:01:51 -0400

    connect to mongoDB

--- server/seed/credential-seeder.js
+++ server/seed/credential-seeder.js
@@ -1,8 +1,8 @@
 var Credential = require('../model/credential');
 var mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost/credentials');
+// mongoose.connect('mongodb://localhost/credentials');
 var fs= require('fs');
-var password = fs.readFileSync('../password.txt');
+var password = fs.readFileSync('../password.txt'); // get password
 mongoose.connect('mongodb://j67shi:'+password+'@jay-shi-shard-00-00-zzlsw.mongodb.net:27017,jay-shi-shard-00-01-zzlsw.mongodb.net:27017,jay-shi-shard-00-02-zzlsw.mongodb.net:27017/test?ssl=true&replicaSet=jay-shi-shard-0&authSource=admin');
 var credential = new Credential({
     username: "jay-shi",
